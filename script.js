6006c74dd287b328465f63e40b0b826311b8cd4b
Abhiru Karki <abhirukarki311@gmail.com>
2024-12-12 10:00:52 +0000

    Modify JS File By Adding Logic for GUI

--- script.js
+++ script.js
@@ -1,68 +1,89 @@
+console.log("First to score 5 wins the Game");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+const ties = document.querySelector("#ties");
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".computerScore");
+let tiesCounter = 0;
+
 let hScore = 0,
   cScore = 0;
+
+const result = document.querySelector(".result");
+
+function addDisabled() {
+  document.querySelector("section").classList.add("disabled");
+}
+
+function removeDisabled() {
+  document.querySelector("section").classList.remove("disabled");
+}
 
 function getComputerChoice() {
   let randomNum = Math.random();
 
   if (randomNum <= 0.33) {
-    return "Rock";
+    return "ROCK🪨";
   } else if (randomNum <= 0.66) {
-    return "Paper";
+    return "PAPER📜";
   } else {
-    return "Scissor";
+    return "SCISSOR✂️";
   }
 }
 
-function getHumanChoice() {
-  let humanChoice;
-  humanChoice = prompt(
-    "Choose between Rock, Paper & Scissor (Case-insensitive):"
-  );
-  return humanChoice;
-}
-
-function playRound(hChoice, cChoice) {
-  if (hChoice == "rock" || hChoice == "paper" || hChoice == "scissor") {
-    console.log(`You chose ${hChoice}`);
-    console.log(`Computer's Choice ${cChoice}`);
+function playRound(hChoice) {
+  let cChoice = getComputerChoice();
+  if (hChoice == "ROCK🪨" || hChoice == "PAPER📜" || hChoice == "SCISSOR✂️") {
+    result.innerHTML = `<p>You chose ${hChoice} and Computer chose ${cChoice}</p>`;
     if (hChoice == cChoice) {
-      console.log("It's a Draw!");
+      result.innerHTML += `<p>IT'S A DRAW!</p>`;
+      ties.style.fontSize = "25px";
+      ties.innerHTML = `<span style = "font-weight: 800";>Ties: ${++tiesCounter}</span>`;
     } else if (
-      (hChoice == "rock" && cChoice == "scissor") ||
-      (hChoice == "scissor" && cChoice == "paper") ||
-      (hChoice == "paper" && cChoice == "rock")
+      (hChoice == "ROCK🪨" && cChoice == "SCISSOR✂️") ||
+      (hChoice == "SCISSOR✂️" && cChoice == "PAPER📜") ||
+      (hChoice == "PAPER📜" && cChoice == "ROCK🪨")
     ) {
-      console.log("You Win!");
+      result.innerHTML += `<p>YOU WIN 🎉</p>`;
       hScore++;
+      playerScore.innerHTML = `<span>Player Score: ${hScore}</span>`;
+      if (hScore == 5) {
+        startButtonArea.style.display = "none";
+        result.innerHTML = `<span style = "font-size: 50px">YAYY! YOU WON 🎉</span>`;
+        result.innerHTML += `<p style = "font-size: 15px">LAST ROUND: You chose ${hChoice} and Computer chose ${cChoice}</p>`;
+        addDisabled();
+        console.log("REFRESH TO RESTART THE GAME");
+      }
     } else {
-      console.log("Computer Wins!");
+      startButtonArea.style.display = "none";
+      result.innerHTML += `<p>COMPUTER WINS 🎉</p>`;
       cScore++;
+      computerScore.innerHTML = `<span>Computer Score: ${cScore}</span>`;
+      if (cScore == 5) {
+        startButtonArea.style.display = "none";
+        result.innerHTML = `<span style = "font-size: 50px">YOU LOSE BOOO!👎</span>`;
+        result.innerHTML += `<p style = "font-size: 15px">LAST ROUND: You chose ${hChoice} and Computer chose ${cChoice}</p>`;
+        addDisabled();
+        console.log("REFRESH TO RESTART THE GAME");
+      }
     }
   } else {
     console.log("Invalid Input");
   }
 }
 
-//*FOR CONSOLE BASED*
-// let hChoice;
-// let cChoice;
+const startButtonArea = document.querySelector(".playButton");
+const startButton = document.querySelector("#play");
 
-// hChoice = getHumanChoice();
-// cChoice = getComputerChoice();
-// cChoice = cChoice.toLowerCase();
-// hChoice = hChoice.toLowerCase();
-// playRound(hChoice, cChoice);
+startButton.addEventListener("click", () => {
+  startButton.style.display = "none";
+  startButtonArea.textContent = "Game Begins ! Start By Choosing Your Move 🎯";
+  startButtonArea.style.fontSize = "20px";
+  startButtonArea.style.margin = "5px";
+  startButtonArea.style.padding = "5px";
+});
 
-// hChoice = getHumanChoice();
-// cChoice = getComputerChoice();
-// cChoice = cChoice.toLowerCase();
-// hChoice = hChoice.toLowerCase();
-// playRound(hChoice, cChoice);
-
-// hChoice = getHumanChoice();
-// cChoice = getComputerChoice();
-// cChoice = cChoice.toLowerCase();
-// hChoice = hChoice.toLowerCase();
-// playRound(hChoice, cChoice);
-
-// console.log(`Your Score: ${hScore} | Computer Score: ${cScore}`);
+rock.addEventListener("click", () => playRound("ROCK🪨"));
+paper.addEventListener("click", () => playRound("PAPER📜"));
+scissor.addEventListener("click", () => playRound("SCISSOR✂️"));
